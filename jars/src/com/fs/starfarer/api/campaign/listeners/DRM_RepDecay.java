30cda1d9e5c7613f795ea16f8a0832228425165d
TobiaFi <tobia.finzi@gmail.com>
2021-07-09 00:30:44 +0200

    v0.1 contains monthly reputation changes that take into account previous month rep and commission status.
    Signed-off-by: TobiaFi <tobia.finzi@gmail.com>

--- jars/src/com/fs/starfarer/api/campaign/listeners/DRM_RepDecay.java
+++ jars/src/com/fs/starfarer/api/campaign/listeners/DRM_RepDecay.java
@@ -3,59 +3,86 @@ package com.fs.starfarer.api.campaign.listeners;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.FactionAPI;
 import com.fs.starfarer.api.impl.campaign.intel.DRM_MonthlyRepDecayIntel;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.fs.starfarer.api.util.Misc;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Set;
+import java.util.*;
 
 public class DRM_RepDecay implements EconomyTickListener {
 
     public FactionAPI playerFaction;
-    public HashMap<FactionAPI, Float> repMap = new HashMap<FactionAPI, Float>();
-    public HashMap<FactionAPI, Integer> tooltipMap = new HashMap<FactionAPI, Integer>();
-
+    public LinkedHashMap<FactionAPI, Float> repMap = new LinkedHashMap<>();
+    public LinkedHashMap<FactionAPI, Float> tooltipMap = new LinkedHashMap<>();
 
     public DRM_RepDecay() {
+        TreeMap<String, FactionAPI> sortingMap = new TreeMap<>();
+
+        //Sorts all factions based on DisplayName
         for (FactionAPI f : Global.getSector().getAllFactions()) {
-            if (f.isPlayerFaction()) {playerFaction = f;}
-            else {repMap.put(f, null);}
+            if (f.isPlayerFaction()) { playerFaction = f; }
+            else { sortingMap.put(f.getDisplayName(), f); }
         }
-        for (FactionAPI f : repMap.keySet()) {repMap.put(f, f.getRelationship(playerFaction.getId()));}
+
+        //Adds factions to repMap in alphabetical order
+        for (String s : sortingMap.keySet()) {
+            FactionAPI f = sortingMap.get(s);
+            repMap.put(f, f.getRelationship(playerFaction.getId()));
+        }
     }
 
+
     public void reportEconomyMonthEnd() {
+        tooltipMap.clear();
+
+        //Iterates through repMap and applies decay to visible factions only, to avoid issues with mods that add factions mid-game
         for (FactionAPI f : repMap.keySet()) {
             if (f.isShowInIntelTab()) { monthlyRepDecay(f); }
         }
-        Global.getSector().getIntelManager().addIntel(new DRM_MonthlyRepDecayIntel(tooltipMap));
+
+        if (!tooltipMap.isEmpty()) {
+            Global.getSector().getIntelManager().addIntel(new DRM_MonthlyRepDecayIntel(tooltipMap));
+        }
     }
 
     private void monthlyRepDecay(FactionAPI f) {
-        //Creating variables
+        //For rep calculation
         float previousRep = repMap.get(f);
+        float repLoss = 0f;
+        FactionAPI commissionFaction = Misc.getCommissionFaction();
+
+        //For tooltip purposes
         int previousRepInt = f.getRepInt(playerFaction.getId());
         int newRepInt;
-        float repLoss = 0f;
 
-        //Decay calculation
-        if (previousRep > 0.25) { repLoss = -  previousRep * 0.10f; }
-        else if (previousRep < -0.25) { repLoss = - previousRep * 0.05f; }
+        //Basic decay calculation
+        if (previousRep > 0.25f) { repLoss = -previousRep * 0.10f; }
+        else if (previousRep < -0.25f) { repLoss = -previousRep * 0.05f; }
 
+        //Different calculation if player is being commissioned
+        if (commissionFaction != null) {
+            if (f.equals(Misc.getCommissionFaction())) {
+                repLoss = -(previousRep - 0.25f) * 0.10f; //Target is commissioning faction
+            } else if (f.isHostileTo(commissionFaction) && f.getRelToPlayer().getRel() != -0.50f) {
+                repLoss = -(previousRep + 0.50f) * 0.05f; //Target is hostile to commissioning faction
+            } else if (f.getRelationshipLevel(commissionFaction).isPositive() && f.getRelToPlayer().getRel() > 0.50f) {
+                repLoss = -previousRep * 0.05f; //Target is allied to commissioning faction and has positive rep with player
+            } else if (f.getRelationshipLevel(commissionFaction).isPositive() && f.getRelToPlayer().getRel() < 0f) {
+                repLoss = -previousRep * 0.10f; //Target is allied to commissioning faction and has negative rep with player
+            }
+        }
 
-        //Making it so current rep can't drop below 25/50/75 if above those thresholds
+        //Current rep doesn't drop below 25/50/75 if above those thresholds at month end (safety net to maintain relationship status)
         float floor = Math.abs(f.getRelationship(playerFaction.getId())) % 0.25f;
         if (floor != 0) { repLoss = Math.signum(repLoss) * Math.min(Math.abs(repLoss), floor); }
 
-        //Updating rep
+        //Updating rep in memory
         f.adjustRelationship(playerFaction.getId(), repLoss);
         newRepInt = f.getRepInt(playerFaction.getId());
 
-        //Updating maps
-        if (previousRepInt - newRepInt != 0) { tooltipMap.put(f, newRepInt - previousRepInt); }
+        //Updating maps. tooltipMap is only updated if the tooltip can show a whole number greater than 0
+        if (previousRepInt - newRepInt != 0) { tooltipMap.put(f, repLoss); }
         repMap.put(f, f.getRelationship(playerFaction.getId()));
     }
 
+    //Necessary for interface implementation
     public void reportEconomyTick(int iterIndex) {}
 }
--- jars/src/com/fs/starfarer/api/impl/campaign/intel/DRM_MonthlyRepDecayIntel.java
+++ jars/src/com/fs/starfarer/api/impl/campaign/intel/DRM_MonthlyRepDecayIntel.java
@@ -1,55 +1,37 @@
 package com.fs.starfarer.api.impl.campaign.intel;
 
 import com.fs.starfarer.api.campaign.FactionAPI;
-import com.fs.starfarer.api.characters.RelationshipAPI;
+import com.fs.starfarer.api.impl.campaign.CoreReputationPlugin;
 import com.fs.starfarer.api.ui.TooltipMakerAPI;
 import com.fs.starfarer.api.util.Misc;
 
-import java.awt.*;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class DRM_MonthlyRepDecayIntel extends BaseIntelPlugin {
 
-    public HashMap<FactionAPI, Integer> tooltipMap;
+    public LinkedHashMap<FactionAPI, Float> tooltipMap = new LinkedHashMap<>();
 
-    public DRM_MonthlyRepDecayIntel(HashMap<FactionAPI, Integer> map) {
-
+    public DRM_MonthlyRepDecayIntel(LinkedHashMap<FactionAPI, Float> map) {
         endingTimeRemaining = 5f;
-        tooltipMap = map;
+        tooltipMap.putAll(map); //Map of rep changes is copied to avoid unintended interactions
     }
 
     @Override
     public void createSmallDescription(TooltipMakerAPI info, float width, float height) {
-        info.addPara("LALALILALA", Misc.getNegativeHighlightColor(), 0f);
+        info.addPara("As your actions lose some of their impact over time, people will slowly forget or forgive what caught their attention in the past."
+                + "\nYour standing with the following factions has changed:",0f);
+        bullet(info);
         for (FactionAPI f : tooltipMap.keySet()) {
-            String article;
-            String name = f.getDisplayName();
-            String rep1 = "bababa";
-            Color good = new Color(147, 242, 0);
-            Color bad = new Color(231, 91, 0);
-            Color[] hl1 = new Color[] {Color.RED, Color.GREEN};
-
-            if (f.getDisplayNameWithArticle().startsWith("the")) { article = "the "; }
-            else { article = ""; }
-/*
-            if (tooltipMap.get(f) > 0) {
-                rep = " improved by " + tooltipMap.get(f).toString();
-                //hl = new Color[] {f.getColor(), good};
-            }
-            else {
-                rep = " decreased by " + tooltipMap.get(f).toString();
-                //hl = new Color[] {f.getColor(), bad};
-            }
-*/
-            //info.addPara(f.getDisplayName() + tooltipMap.get(f).toString(), f.getColor(), 0f);
-            info.addPara("Relationship with " + article + name + rep1, 0f, hl1, name, rep1);
-
+            CoreReputationPlugin.addAdjustmentMessage(tooltipMap.get(f), f, null,
+                    null, null, info, Misc.getTextColor(), false, 10f);
         }
+        unindent(info);
     }
 
     protected String getName() {
         return "Monthly reputation report";
     }
 
+    //TODO: find a proper icon
     public String getIcon() {return "graphics/icons/intel/reputation.png";}
 }
